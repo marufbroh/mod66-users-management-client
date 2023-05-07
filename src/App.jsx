c4ec04ccd5d5f42c0724872a57eb7f894ae68214
marufbroh <marufmd775@gmail.com>
2023-05-07 18:04:59 +0600

    client site final commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import './App.css'
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers)
+
+        form.reset()
+      })
+  }
+
+  return (
+    <div>
+      <h2>User management system</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      {
+        users.map(user => <p key={user.id}>{user.id}, {user.name}, {user.email}</p>)
+      }
+    </div>
+  );
+};
+
+export default App;
